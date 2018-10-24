a2b7f84b06a5cf56e79576746e42934563eb473a
GlennHulscher <hulscher12@gmail.com>
2018-10-24 20:48:04 +0200

    6

---
+++ afvink6/src/Afvink6.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package afvink6;
+
+public class Afvink6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame.main(args);
+    }
+
+}
